11fb72f35381622e52599c6b66b5e54a3f2247a2
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2020-04-10 00:02:38 -0700

    auto select time slots and submit

--- whole_foods_delivery_slot/contentScript.js
+++ whole_foods_delivery_slot/contentScript.js
@@ -1,16 +1,14 @@
-const HTML_SELECTOR = 'div.ufss-slotselect-container';
-const NOT_AVAILABLE = 'No delivery windows available';
+const SLOT_SELECTOR = 'div.ufss-slot.ufss-available';
+const SUBMIT_SELECTOR = 'input[type=submit]';
 const REFRESH_SECONDS = 10;
-const EXPIRE_SECONDS = 120;
 
-const slot = document.querySelector(HTML_SELECTOR).innerText;
-console.log(`${new Date().toLocaleTimeString()}: ${slot}`);
-
-found = !slot.startsWith(NOT_AVAILABLE);
-if (found) {
-    chrome.runtime.sendMessage(slot);
+const slot = document.querySelector(SLOT_SELECTOR);
+if (slot) {
+    chrome.runtime.sendMessage(slot.innerText);
+    setTimeout(() => {
+        slot.click();
+        document.querySelector(SUBMIT_SELECTOR).click();
+    }, 1000);
+} else {
+    setTimeout(() => { location.reload(); }, REFRESH_SECONDS * 1000);
 }
-
-setTimeout(() => {
-    location.reload();
-}, (found ? EXPIRE_SECONDS : REFRESH_SECONDS) * 1000);
