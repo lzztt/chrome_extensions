595a50b491581692c05b1e18c4cdeed8ab6d1719
Long <15257546+lzztt@users.noreply.github.com>
2020-04-09 18:16:54 -0700

    use notification instead of alert

--- chrome_extension/whole_foods_delivery_slot.js
+++ chrome_extension/whole_foods_delivery_slot.js
@@ -1,12 +1,16 @@
 const HTML_SELECTOR = 'div.ufss-slotselect-container';
 const NOT_AVAILABLE = 'No delivery windows available';
 const REFRESH_SECONDS = 10;
+const EXPIRE_SECONDS = 120;
 
 const slot = document.querySelector(HTML_SELECTOR).innerText;
 console.log(`${new Date().toLocaleTimeString()}: ${slot}`);
 
-if (!slot.startsWith(NOT_AVAILABLE)) {
-    alert(slot);
-} else {
-    setTimeout(() => { location.reload(); }, REFRESH_SECONDS * 1000);
+found = !slot.startsWith(NOT_AVAILABLE);
+if (found) {
+    chrome.runtime.sendMessage(slot);
 }
+
+setTimeout(() => {
+    location.reload();
+}, (found ? EXPIRE_SECONDS : REFRESH_SECONDS) * 1000);
