5caaa667f79fe7f1eafab314b9ec88b04ce268f2
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2020-04-09 19:01:48 -0700

    update icon url

--- whole_foods_delivery_slot/background.js
+++ whole_foods_delivery_slot/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(slot => {
     chrome.notifications.create({
         type: "basic",
         title: "Delivery Window Found",
-        iconUrl: "animals.svg",
+        iconUrl: "icon.svg",
         message: slot,
     });
 });
